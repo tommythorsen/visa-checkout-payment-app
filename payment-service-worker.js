737c11bdeaedb27ca81fc4e5707d1e295659cb9b
Tommy Thorsen <tommyt@opera.com>
2016-05-20 14:26:20 +0200

    Try to pass a payment request back from the service worker

--- payment-service-worker.js
+++ payment-service-worker.js
@@ -3,11 +3,15 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log("Handling fetch event: " + event.request.url);
-    console.log("method: " + event.request.method);
-    //console.log("BODY: " + event.request.formData());
+    console.log("Handling fetch event: " + event.request.method + " " + event.request.url);
 });
 
 self.addEventListener('message', function(event) {
     console.log("Got message: " + event);
+    // Post a dummy payment request. This does not attempt to look anything
+    // like a proper webpayments request, it's just a placeholder.
+    event.ports[0].postMessage({
+        currencyCode: "NOK",
+        total: "100.0"
+    });
 });
